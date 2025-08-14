3f14bc90be3d3ae5f5c88e04704a9a7461005172
Pizofh <stevegarnicas@hotmail.com>
2025-08-14 14:50:23 -0500

    feat: entrypoint with collectstatic + migrate + gunicorn

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# 1) Recolecta estáticos para WhiteNoise
+python manage.py collectstatic --noinput
+
+# 2) Aplica migraciones
+python manage.py migrate --noinput
+
+# 3) Arranca gunicorn escuchando en $PORT (Render lo inyecta)
+exec gunicorn SmartForm.wsgi:application --bind 0.0.0.0:$PORT
