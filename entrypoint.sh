b4973270ada6bf0e13ff4507bfcaafc5d73f69f3
Pizofh <stevegarnicas@hotmail.com>
2025-08-14 15:48:30 -0500

    Entrypoint modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ python manage.py collectstatic --noinput
 # 2) Aplica migraciones
 python manage.py migrate --noinput
 
+python manage.py loaddata superuser.json
+
 # 3) Arranca gunicorn escuchando en $PORT (Render lo inyecta)
 exec gunicorn SmartForm.wsgi:application --bind 0.0.0.0:$PORT
